082290102f06d26f9a6d5f1cf6c543e8fc56b493
Sally Chhuon <schhuo2@gmail.com>
2020-05-24 21:36:19 -0500

    Created frame of calculator display

---
+++ calculator.js
@@ -0,0 +1,37 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x/y;
+}
+
+function operate(operator, x, y) {
+
+    //switch statement to determine which function to execute based on operation
+    switch(operator) {
+        case '+':
+            add(x, y);
+            break;
+        case '-':
+            subtract(x, y);
+            break;
+        case '*':
+            multiply(x, y);
+            break;
+        case '/':
+            divide(x,y);
+            break;
+        default:
+        ; 
+    }
+    
+}
